ba829a3e0bf650173afcef255ee654d2273e9fcf
doanhuyxh <doanhuyxh@gmail.com>
2023-05-12 01:14:05 +0700

    đang làm phần đăng nhập

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 using WebApp.Data;
 using WebApp.Models;
 using WebApp.Models.AccountViewModels;
@@ -43,6 +46,38 @@ namespace WebApp.Controllers
 
             if (result.Succeeded)
             {
+                var user = await _userManager.FindByNameAsync(model.UserName);
+                var claims = new List<Claim> {
+                new Claim(ClaimTypes.Name, user.UserName),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, "UserRole")
+
+                };
+
+                // Xây dựng ClaimsIdentity
+                var claimsIdentity = new ClaimsIdentity(
+                    claims, CookieAuthenticationDefaults.AuthenticationScheme);
+
+                // Thiết lập các thuộc tính xác thực (nếu có)
+                var authProperties = new AuthenticationProperties
+                {
+                    IsPersistent = true, // Thiết lập cho phép lưu cookie vĩnh viễn (remember me)
+                    ExpiresUtc = DateTimeOffset.UtcNow.AddHours(2), // Thiết lập thời gian hết hạn sau 2 giờ
+                };
+
+                // Đăng nhập người dùng và tạo cookie
+                await HttpContext.SignInAsync(
+                    CookieAuthenticationDefaults.AuthenticationScheme,
+                    new ClaimsPrincipal(claimsIdentity),
+                    authProperties);
+
+                await HttpContext.Response.Cookies.AuthenticateAsync(
+                      CookieAuthenticationDefaults.CookiePrefix + CookieAuthenticationDefaults.AuthenticationScheme,
+                       await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme),
+                        new CookieOptions  {
+        // Cấu hình các thuộc tính của cookie (nếu cần)
+                        });
+
                 return RedirectToAction("Index", "Dashboard", new { area = "Admin" });
             }
             else
@@ -73,6 +108,15 @@ namespace WebApp.Controllers
                 }
             }
             return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync(); // Đăng xuất người dùng
+
+            return RedirectToAction("Index", "Home"); // Chuyển hướng đến trang chủ hoặc trang khác
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
@@ -63,6 +64,33 @@ namespace WebApp
                 options.User.RequireUniqueEmail = false;
             });
 
+            //thêm session
+            builder.Services.AddSession(options =>
+            {
+                options.Cookie.Name = "Tên phiên";
+                options.IdleTimeout = TimeSpan.FromMinutes(1); // Thời gian không hoạt động trước khi phiên hết hạn
+                options.Cookie.HttpOnly = true; // Chỉ cho phép truy cập thông qua HTTP
+                options.Cookie.IsEssential = true; // Đảm bảo phiên vẫn hoạt động ngay cả khi người dùng không đồng ý cookie
+            });
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+            }).AddCookie(options =>
+            {
+                options.Cookie.Name = "MyCookieName"; // Tên cookie
+                options.Cookie.Domain = "example.com"; // Tên miền cookie áp dụng (nếu có)
+                options.Cookie.Path = "/"; // Đường dẫn cookie áp dụng
+                options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest; // Chính sách bảo mật (SameAsRequest, Always, None)
+                options.Cookie.HttpOnly = true; // Cookie chỉ được truy cập bằng HTTP (không bằng JavaScript)
+                options.Cookie.SameSite = SameSiteMode.Strict; // Giới hạn cookie chỉ được gửi trong cùng nguồn (Strict, Lax, None)
+
+                options.ExpireTimeSpan = TimeSpan.FromDays(7); // Thời gian hết hạn của cookie
+                options.SlidingExpiration = true; // Cho phép cập nhật thời gian hết hạn khi có hoạt động từ người dùng
+                options.AccessDeniedPath = "/Account/AccessDenied"; // Đường dẫn chuyển hướng khi truy cập bị từ chối
+            });
+
 
             var app = builder.Build();
 
@@ -75,7 +103,7 @@ namespace WebApp
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            app.UseSession();
             app.UseRouting();
             app.UseAuthentication();
             app.UseAuthorization();
@@ -83,6 +111,9 @@ namespace WebApp
 
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapControllers();
+                endpoints.MapRazorPages();
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
@@ -92,7 +123,7 @@ namespace WebApp
                     pattern: "Admin/{controller=Dashboard}/{action=Index}/{id?}",
                     defaults: new { area = "Admin" });
 
-                
+
             });
 
             using (var scope = app.Services.CreateScope())
