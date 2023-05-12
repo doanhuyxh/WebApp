34f0140fe1b85ea8509fdf2ac1651cab810a4b4d
doanhuyxh <doanhuyxh@gmail.com>
2023-05-12 10:58:38 +0700

    xóa cache

--- Models/ApplicationUser .cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity;
-
-namespace WebApp.Models
-{
-    public class ApplicationUser : IdentityUser
-    {
-
-    }
-}
--- Models/EntityBase.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApp.Models
-{
-    public class EntityBase
-    {
-        public bool IsDeleted { get; set; }
-        public string CreatedBy { get; set; } = string.Empty;
-        public DateTime CreatedDate { get; set; }
-    }
-}
