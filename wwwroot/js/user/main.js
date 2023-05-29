790ebb85892d66ffc072163c6c248d5e3f630511
DoanQuangHuy <doanhuyxh@gmail.com>
2023-05-29 15:41:27 +0700

    up

--- wwwroot/js/user/main.js
+++ wwwroot/js/user/main.js
@@ -16,7 +16,18 @@ $(document).ready(function () {
             $('#Fb_href').attr('href', `${data.object.linkChatFaceBook}`).attr('target', '_blank');
             $('#address_footer').html(data.object.address);
 
+            $('#MailFooter').attr('href', `mailTo:${data.object.mail}`).attr('target', '_blank');
+            $('#MailFooter').html(data.object.mail);
+
 
         })
     //các code khác viết ở dưới
+    $(".owl-carousel").owlCarousel({
+        items: 4, // Số slide hiển thị trên một lần cuộn
+        loop: true, // Lặp lại carousel
+        nav: true, // Hiển thị nút điều hướng
+        dots: true,
+        autoplay: true // Hiển thị điểm đánh dấu
+        // Các tùy chọn khác cho Owl Carousel 2
+    });
 });
