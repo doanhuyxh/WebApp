47ff185b6cdfcc435c7bd0a4f35b511f63b2da40
DoanQuangHuy <doanhuyxh@gmail.com>
2023-05-29 08:41:00 +0700

    up

--- wwwroot/js/user/main.js
+++ wwwroot/js/user/main.js
@@ -6,11 +6,15 @@ $(document).ready(function () {
         .then(data => {
             document.title = data.object.titleWeb;
             $("#Logo_src").attr("src", data.object.loGo);
+            $("#Logo_footer").attr("src", data.object.loGo);
 
             $('#tel_href').attr('href', `tel:${data.object.phoneNumber}`);
             $('#tel_number').html('<strong>' + data.object.phoneNumber + '</strong>');
+            $('#PhoneFooter').attr('href', `tel:${data.object.phoneNumber}`);
+            $('#PhoneFooter').html(data.object.phoneNumber);
 
             $('#Fb_href').attr('href', `${data.object.linkChatFaceBook}`).attr('target', '_blank');
+            $('#address_footer').html(data.object.address);
 
 
         })
